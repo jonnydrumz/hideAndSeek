19182c0ec44ff5d12d648a101e16ae523f642e0b
DavidKBD <davidkbd@gmail.com>
2021-01-29 23:13:25 +0100

    asdlfadf

--- UI/SetupPlayer/SetupPlayer.gd
+++ UI/SetupPlayer/SetupPlayer.gd
@@ -6,6 +6,10 @@ func update_player_data(player_data):
 	$Wait.hide()
 	$TypeLabel.text = player_data.name
 
+func update_player_list(players):
+	for player in players:
+		$ItemList.add_item(player.name)
+
 func _on_Button_pressed():
 	emit_signal("on_start_pressed", $LineEdit.text)
 	$Button.hide()
