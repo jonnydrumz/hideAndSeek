c1c760f53a5f8e4ac4f0445145b1343856ae1421
davidkbd <davidkbd@gmail.com>
2021-01-30 16:54:12 +0100

    musica

--- GhostBusterManager.gd
+++ GhostBusterManager.gd
@@ -6,6 +6,6 @@ func busted(ghost):
 	add_child(tween)
 	tween.connect("tween_all_completed", tween, "queue_free")
 	tween.connect("tween_all_completed", ghost, "queue_free")
-	tween.interpolate_property(ghost, "scale:y",
+	tween.interpolate_property(ghost, "scale:x",
 			ghost.scale.x, .0, .5, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 	tween.start()
