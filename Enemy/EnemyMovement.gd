7e3e10885d29a25d039714ddd6fc1f22ffb0ed43
jonnydrumz <jonnydrumz@gmail.com>
2021-01-30 19:06:08 +0100

    Pisadas enemigo

--- Enemy/EnemyMovement.gd
+++ Enemy/EnemyMovement.gd
@@ -1,39 +1,52 @@
 extends KinematicBody2D
 
 export(float) var seconds_to_arrive : float = 30.0
+export(PackedScene) var steps       : PackedScene
+
+export var transition_duration = 1.00
+export var transition_type = 1 # TRANS_SINE
 
 onready var tween := $Tween
+onready var steps_counter = 0
 var tombs
 var current_tomb
 var rng = RandomNumberGenerator.new()
 var seconds
 
+
 func _ready():
 	tombs = get_tree().get_nodes_in_group("TOMB")
-	rng.randomize()
-	current_tomb = tombs[rng.randi()%tombs.size()]
-	var length = position.distance_to(current_tomb.position)
-	seconds = position.distance_to(current_tomb.position) / seconds_to_arrive
-	tween.interpolate_property(self, "position", position, current_tomb.position, seconds, Tween.TRANS_CUBIC, Tween.EASE_OUT)
-	tween.start()
-
+	movement()
+	$TimerSteps.start()
 
 func _on_Tween_tween_all_completed():
-	$Timer.start(rand_range(5.0,10.0))
-
-	# Go to a new tomb
-#	rng.randomize()
-#	position = current_tomb.position
-#	current_tomb = tombs[rng.randi()%tombs.size()]
-#	var length = position.distance_to(current_tomb.position)
-#	seconds = position.distance_to(current_tomb.position) / seconds_to_arrive
-	
+	$Timer.start(rand_range(5.0,15.0))
+	$AudioStreamPlayer2D.stop()
 
 
 func _on_Timer_timeout():
+	movement()
+	
+
+func movement():
 	rng.randomize()
+	$AudioStreamPlayer2D.play()
 	current_tomb = tombs[rng.randi()%tombs.size()]
-	var length = position.distance_to(current_tomb.position)
-	seconds = position.distance_to(current_tomb.position) / seconds_to_arrive
-	tween.interpolate_property(self, "position", position, current_tomb.position, seconds, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	var length = position.distance_to(current_tomb.global_position)
+	seconds = position.distance_to(current_tomb.global_position) / seconds_to_arrive
+	tween.interpolate_property(self, "position", position, current_tomb.global_position, seconds, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	tween.interpolate_property($AudioStreamPlayer2D, "volume_db", 0, -10, transition_duration, transition_type, Tween.EASE_IN, 0)
 	tween.start()
+
+
+func _on_TimerSteps_timeout():
+	var step = steps.instance()
+	if (steps_counter % 2 == 0):
+		step.is_left = true
+	else:
+		step.is_left = false
+	steps_counter = steps_counter + 1
+	step.global_position = global_position
+	step.rotation = current_tomb.rotation
+	get_parent().add_child(step)
+
