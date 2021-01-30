97f35eba4ff3d65dfea1168e90709ceeb6329180
davidkbd <davidkbd@gmail.com>
2021-01-30 12:46:37 +0100

    asdf

--- PlayerBuscador/PlayerBuscador.gd
+++ PlayerBuscador/PlayerBuscador.gd
@@ -1,17 +1,18 @@
 extends KinematicBody2D
 
+export(float) var player_speed : float = 64.0
+
 onready var velocity : Vector2 = Vector2.ZERO
+onready var player_speed_y : float = player_speed * .5
 
 func _physics_process(delta : float):
 	_move(delta)
 	velocity = move_and_slide(velocity, Vector2.UP, false, 4.0)
 
 func _move(delta : float):
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -64.0
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x = 64.0
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -32.0
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 32.0
+	var leftv = -player_speed if Input.is_action_pressed("ui_left") else .0
+	var rightv = player_speed if Input.is_action_pressed("ui_right") else .0
+	var upv = -player_speed_y if Input.is_action_pressed("ui_up") else .0
+	var downv = player_speed_y if Input.is_action_pressed("ui_down") else .0
+	velocity.x = leftv + rightv
+	velocity.y = upv + downv
