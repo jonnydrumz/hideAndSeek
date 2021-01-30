558f0f6fe13c9b2d5d28d2610f6ee399c9011876
davidkbd <davidkbd@gmail.com>
2021-01-30 14:42:14 +0100

    ghostbuster

--- GhostBusterManager.gd
+++ GhostBusterManager.gd
@@ -1,4 +1,11 @@
 extends Node
 
+
 func busted(ghost):
-	print(ghost)
+	var tween := Tween.new()
+	add_child(tween)
+	tween.connect("tween_all_completed", tween, "queue_free")
+	tween.connect("tween_all_completed", ghost, "queue_free")
+	tween.interpolate_property(ghost, "scale:y",
+			ghost.scale.x, .0, .5, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
+	tween.start()
