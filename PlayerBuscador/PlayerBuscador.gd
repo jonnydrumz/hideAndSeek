c5a5ebc4f80453bf22f08274c0e7401c962929a7
davidkbd <davidkbd@gmail.com>
2021-01-30 13:11:54 +0100

    luces

--- PlayerBuscador/PlayerBuscador.gd
+++ PlayerBuscador/PlayerBuscador.gd
@@ -2,11 +2,20 @@ extends KinematicBody2D
 
 export(float) var player_speed : float = 64.0
 
+onready var lamplight : Light2D = $LampLight
+
 onready var velocity : Vector2 = Vector2.ZERO
+onready var angle    : float   = .0
 onready var player_speed_y : float = player_speed * .5
 
 func _physics_process(delta : float):
 	_move(delta)
+	_action(delta)
+	if velocity.length() > .1:
+		angle = velocity.angle()
+	if lamplight.running:
+		lamplight.rotation = angle
+		
 	velocity = move_and_slide(velocity, Vector2.UP, false, 4.0)
 
 func _move(delta : float):
@@ -16,3 +25,10 @@ func _move(delta : float):
 	var downv = player_speed_y if Input.is_action_pressed("ui_down") else .0
 	velocity.x = leftv + rightv
 	velocity.y = upv + downv
+
+func _action(delta : float):
+	if Input.is_action_just_pressed("ui_accept"):
+		_lamp_turn_on()
+
+func _lamp_turn_on():
+	lamplight.turn_on()
