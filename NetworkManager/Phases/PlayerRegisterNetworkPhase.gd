9d732cf209be2701ede61d5d50fcb4bf75a60b98
davidkbd <davidkbd@gmail.com>
2021-01-30 11:55:26 +0100

    asdfasdf

--- NetworkManager/Phases/PlayerRegisterNetworkPhase.gd
+++ NetworkManager/Phases/PlayerRegisterNetworkPhase.gd
@@ -5,19 +5,19 @@ onready var host = get_parent()
 func name_entered(nickname):
 	var id = get_tree().get_network_unique_id()
 	if get_tree().is_network_server():
-		set_player_name(id, nickname)
+		set_player_name(id, nickname, host.players)
 	else:
-		rpc_id(1, "set_player_name", id, nickname)
+		rpc_id(1, "set_player_name", id, nickname, host.players)
 
-remote func set_player_name(id, nickname):
-	for player in host.players:
+remote func set_player_name(id, nickname, players):
+	for player in players:
 		if player.id == id:
 			player.nickname = nickname
 	if get_tree().is_network_server():
-		update_player_data(host.players)
-	for player in host.players:
+		update_player_data(players)
+	for player in players:
 		if player.id != 1:
-			rpc_id(player.id, "update_player_data", host.players)
+			rpc_id(player.id, "update_player_data", players)
 	
 remote func update_player_data(players):
 	host.emit_signal("player_data_received", players)
