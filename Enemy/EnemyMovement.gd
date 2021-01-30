13bd34ee0373509ccf634a2937df9e9b7be678d2
jonnydrumz <jonnydrumz@gmail.com>
2021-01-30 16:49:41 +0100

    Cambios en movimiento enemigo

--- Enemy/EnemyMovement.gd
+++ Enemy/EnemyMovement.gd
@@ -1,73 +1,30 @@
 extends KinematicBody2D
 
+export(float) var seconds_to_arrive : float = 30.0
+
+onready var tween := $Tween
 var tombs
 var current_tomb
-
-# Random number generator
 var rng = RandomNumberGenerator.new()
-
-# Movement variables
-export var speed = 25
-var direction : Vector2
-var last_direction = Vector2(0, 1)
-var bounce_countdown = 0
+var seconds
 
 func _ready():
 	tombs = get_tree().get_nodes_in_group("TOMB")
-	current_tomb = tombs[randi()%tombs.size()]
-	print(current_tomb)
+	rng.randomize()
+	current_tomb = tombs[rng.randi()%tombs.size()]
+	var length = position.distance_to(current_tomb.position)
+	seconds = position.distance_to(current_tomb.position) / seconds_to_arrive
+	set_process(true)
+	
+func _process(delta):
+	tween.interpolate_property(self, "position", position, current_tomb.position, seconds, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	tween.start()
 
-func _on_Timer_timeout():
-	var tomb_relative_position = current_tomb.position - position
-	
-	if tomb_relative_position.length() <= 16:
-		direction = Vector2.ZERO
-		last_direction = tomb_relative_position.normalized()
-	elif tomb_relative_position.length() <= 100 and bounce_countdown == 0:
-		# If player is within range, move toward it
-		direction = tomb_relative_position.normalized()
-	elif bounce_countdown == 0:
-		# If player is too far, randomly decide whether to stand still or where to move
-		var random_number = rng.randf()
-		if random_number < 0.05:
-			direction = Vector2.ZERO
-		elif random_number < 0.1:
-			direction = Vector2.DOWN.rotated(rng.randf() * 2 * PI)
-	
-	if bounce_countdown > 0:
-		bounce_countdown = bounce_countdown - 1
-
-func _physics_process(delta):
-	var movement = direction * speed * delta
-	
-	var collision = move_and_collide(movement)
-	
-	if collision != null and collision.collider.name != "Player":
-		direction = direction.rotated(rng.randf_range(PI/4, PI/2))
-		bounce_countdown = rng.randi_range(2, 5)
-
-func get_animation_direction(direction: Vector2):
-	var norm_direction = direction.normalized()
-	if norm_direction.y >= 0.707:
-		return "down"
-	elif norm_direction.y <= -0.707:
-		return "up"
-	elif norm_direction.x <= -0.707:
-		return "left"
-	elif norm_direction.x >= 0.707:
-		return "right"
-	return "down"
-	
-func animates_monster(direction: Vector2):
-	if direction != Vector2.ZERO:
-		last_direction = direction
-		
-		# Choose walk animation based on movement direction
-		var animation = get_animation_direction(last_direction) + "_walk"
-		
-		# Play the walk animation
-		$AnimatedSprite.play(animation)
-	else:
-		# Choose idle animation based on last movement direction and play it
-		var animation = get_animation_direction(last_direction) + "_idle"
-		$AnimatedSprite.play(animation)
+func _on_Tween_tween_completed(object, key):
+	# Go to a new tomb
+	rng.randomize()
+	position = current_tomb.position
+	current_tomb = tombs[rng.randi()%tombs.size()]
+	var length = position.distance_to(current_tomb.position)
+	seconds = position.distance_to(current_tomb.position) / seconds_to_arrive
+	tween.stop(object)
