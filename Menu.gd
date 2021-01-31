9db59ac2c4a5ff839583ea518337f66ed383e11e
jonnydrumz <jonnydrumz@gmail.com>
2021-01-31 03:11:58 +0100

    Menu

--- Menu.gd
+++ Menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
 func _on_Iniciar_pressed():
+	LevelConfiguration.map_height = $MapHeight.value
+	LevelConfiguration.map_width = $MapWidth.value
+	LevelConfiguration.enemies_count = $EnemyNumber.value
+	LevelConfiguration.objects_amount = $GraveAmount.value
 	get_tree().change_scene("Main.tscn")
 
